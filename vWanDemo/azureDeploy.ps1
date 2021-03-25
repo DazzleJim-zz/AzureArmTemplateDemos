a5c612e35bcf6ae1a36ecc49cacb58b3ad841de2
Darren Johns <dazzlejim@hotmail.com>
2021-03-25 15:00:10 +1300

    Delete azureDeploy.ps1

--- vWanDemo/azureDeploy.ps1
+++
@@ -1,6 +0,0 @@
-# Login to Azure
-Connect-AzAccount
-
-# Provision a new virtual WAN and Hub
-New-AzResourceGroup -Name rg-demo-securehub -Location australiaeast
-New-AzResourceGroupDeployment -ResourceGroupName rg-demo-securehub -TemplateFile "azuredeploy.json"-TemplateParameterFile "azuredeploy.parameters.json"
